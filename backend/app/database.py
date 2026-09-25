d00e759c9ce3a74a34e59f12abe20b5b4a642914
sudhanshu kumar <shudhansukuma1081@gmail.com>
2026-09-25 17:53:33 +0530

    add env support

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Yahan apna postgres password daalo (jo install ke time set kiya tha)
-DATABASE_URL = "postgresql://postgres:Qazwsx230408%40@localhost:5432/job_portal_db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:Qazwsx230408%40@localhost:5432/job_portal_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
