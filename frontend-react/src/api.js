af1b25b2ce5ed8b7d2929d012a96fded2a7077dc
sudhanshu kumar <shudhansukuma1081@gmail.com>
2026-09-26 16:45:01 +0530

    update API URL for production

--- frontend-react/src/api.js
+++ frontend-react/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://job-resume-matcher-7159.onrender.com";
 
 async function request(path, options = {}) {
   const res = await fetch(`${API_URL}${path}`, {
