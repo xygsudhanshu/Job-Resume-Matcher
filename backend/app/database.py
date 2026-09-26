2e75a1ddde8986d9930bbac8939701c830923c69
sudhanshu kumar <shudhansukuma1081@gmail.com>
2026-09-26 16:31:34 +0530

    fix python version and db driver

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:Qazwsx230408%40@localhost:5432/job_portal_db")
-
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
